bec733dba8abd81485a6773737af242bd09a919a
Jose Soto <jsoto7087@gmail.com>
2023-02-08 09:58:02 -0500

    commit tree c30d2544aad12081dc0e8ee4ee1628a16ee3b32c

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,10 @@
 class Network < ActiveRecord::Base
+
+    has_many :shows
+    has_many :characters, through: :shows
+
+    def sorry
+        return  "We're sorry about passing on John Mulaney's pilot"
+    end 
   
 end
